cb77b0040d1adca65ddfee7d6f8a45eff20f38c2
Lyubov Myetolkina <luba150278@gmail.com>
2021-02-17 15:41:01 +0200

    tasks 13 inclusive completed

---
+++ js/task13.js
@@ -0,0 +1,21 @@
+let localSt = document.getElementById("textForLocalStorage");
+let sessionSt = document.getElementById("textForSessionStorage");
+let cookiesStr = document.getElementById("textForCookies");
+function inputData() {
+  let localStVal = localSt.value;
+  let sessionStVal = sessionSt.value;
+  let cookiesVal = cookiesStr.value;
+  localStorage.setItem("LocalStorage", localStVal);
+  localStorage.setItem("SessionStorage", sessionStVal);
+
+  document.cookie = "cookiesStr=" + cookiesVal;
+}
+window.onload = function () {
+  localSt.innerHTML = localStorage.getItem("LocalStorage");
+  sessionSt.innerHTML = localStorage.getItem("SessionStorage");
+  let results = document.cookie.replace(
+    /(?:(?:^|.*;\s*)cookiesStr\s*\=\s*([^;]*).*$)|^.*$/,
+    "$1"
+  );
+  cookiesStr.innerHTML = results;
+};
